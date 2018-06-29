c6feb863e0bb92f210367aea8f2c0aabdb16c2f2
volatiana <volatiana.helisoa@yahoo.fr>
2018-06-29 17:38:17 +0300

    list cmde

--- RedactApplication/RedactApplication/Scripts/codescript.js
+++ RedactApplication/RedactApplication/Scripts/codescript.js
@@ -39,6 +39,22 @@ $(document).ready(function () {
             }
         });
     });
+
+    $('#myDataTable').DataTable({
+        "dom": 'ftipr',
+        "searching": false,
+        columnDefs: [{
+            orderable: false,
+            className: 'select-checkbox',
+            targets: 0
+        }],
+        select: {
+            style: 'os',
+            selector: 'td:first-child'
+        },
+        order: [[1, 'asc']]
+    });
+
 });
 
 
