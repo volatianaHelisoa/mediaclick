fb6e069f30ce03f72fd2c598cad95e3aef9f351e
volatiana <volatiana.helisoa@yahoo.fr>
2018-07-19 13:39:36 +0300

    tempate theme

---
+++ Publish/Scripts/codetemplate.js
@@ -0,0 +1,57 @@
+﻿var showThumb = (function (e) {
+    if (typeof FileReader == "undefined") return true;
+    var elem = $(this);
+    var files = e.target.files;
+    for (var i = 0, f; f = files[i]; i++) {
+        if (f.type.match('image.*')) {
+            var reader = new FileReader();
+            var previewDiv = $('.thumbnail', elem.parent());
+            reader.onload = (function (theFile) {
+                return function (e) {
+                    var image = e.target.result;
+                    if ($(previewDiv).children().length) {
+                        console.log('efa misy image ato');
+                        previewDiv.children().remove();
+                        previewDiv.append('<img src="' + image + '" />');
+                    }
+
+                    else {
+                        previewDiv.append('<img src="' + image + '"/>');
+                    }
+                };
+            })(f);
+            reader.readAsDataURL(f);
+        }
+    }
+});
+var showThumbPhotos = (function (e) {
+    if (typeof FileReader == "undefined") return true;
+    var elem = $(this);
+    var files = e.target.files;
+    for (var i = 0, f; f = files[i]; i++) {
+        if (f.type.match('image.*')) {
+            var reader = new FileReader();
+            var previewDiv = $('.thumbnail-photo', elem.parent());
+            reader.onload = (function (theFile) {
+                return function (e) {
+                    var image = e.target.result;
+                    if ($(previewDiv).children().length) {
+                        console.log('efa misy image ato');
+                        previewDiv.children().remove();
+                        previewDiv.append('<img src="' + image + '" />');
+                    }
+
+                    else {
+                        previewDiv.append('<img src="' + image + '"/>');
+                    }
+                };
+            })(f);
+            reader.readAsDataURL(f);
+        }
+    }
+});
+$('input[type=file]#logoUrl').bind("change", showThumb);
+$('input[type=file]#menu1_paragraphe1_photoUrl').bind("change", showThumbPhotos);
+$('input[type=file]#menu1_paragraphe2_photoUrl').bind("change", showThumbPhotos);
+$('input[type=file]#photo-a-la-une').bind("change", showThumbPhotos);
+$('input[type=file]#menu4_paragraphe2_photoUrl').bind("change", showThumbPhotos);
