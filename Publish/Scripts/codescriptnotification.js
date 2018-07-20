b2e3e973b74c790073d88874b2615cca6789a24a
Unknown <zo.ravalitera@outlook.com>
2018-07-20 14:12:39 +0300

    Target="_blank" removed from notification's link

--- Publish/Scripts/codescriptnotification.js
+++ Publish/Scripts/codescriptnotification.js
@@ -52,7 +52,7 @@
                         
                         //tr_str += '<li class="recent"><a href="' + link + '" id="submit-link"> La commande #' + value.commanderef + ' a été mise à jour par ' + value.fromUserName + ' le ' + dateStr + ' .</a></li>';
 
-                        tr_str += '<li class="recent"><a href="http://'+ link +'" target="_blank" > ' + value.message + '</a></li>';
+                        tr_str += '<li class="recent"><a href="http://'+ link +'"> ' + value.message + '</a></li>';
                     });
                 
                     $('#notiContent').html(tr_str);
